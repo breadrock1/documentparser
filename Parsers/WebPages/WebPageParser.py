821a733c1efbb1c4e9fd1961ba264f1b58621674
breadrock <breadrock1@gmail.com>
2021-06-27 14:20:40 +0300

    Save point to WebPageParser

--- Parsers/WebPages/WebPageParser.py
+++ Parsers/WebPages/WebPageParser.py
@@ -1,17 +1,34 @@
-from typing import Dict
-from requests import get
+from time import time_ns
+from typing import Dict, Optional
+from requests import get, Response
 from bs4 import BeautifulSoup
-from logging import exception, info
+from logging import exception
 
 
 class WebPageParser(object):
-    @staticmethod
-    def __send_get_request(url: str):
+    def __init__(self):
+        pass
+
+    def __write_dom_object_to_file(self, content: Optional[bytes]) -> str:
+
+        def __generate_file_name() -> str:
+            return f'{time_ns()}_webpage.html'
+
+        file_name = __generate_file_name()
+
+        with open(f'/tmp/{file_name}', 'w+') as file:
+            file.write(content.decode('utf-8'))
+            file.close()
+
+        return file_name
+
+    # TODO: How include ssl-cert??
+    def __send_get_request(self, url: str) -> Response or None:
         try:
             response = get(
                 url=url,
-                cert=('/Path/To/File.key', '/Path/to/File.perm'),
-                allow_redirects=True
+                allow_redirects=True,
+                verify=False
             )
         except Exception as e:
             exception(msg=f'[!]\tFailed to send request: {e}')
@@ -20,11 +37,15 @@ class WebPageParser(object):
         if response.status_code != 200:
             return None
 
-    @staticmethod
-    def extract_text_from_webpage(url_address: str) -> Dict[str, str or Dict]:
-        html = WebPageParser.__send_get_request(url=url_address)
+        return response.content
 
-        soup = BeautifulSoup(html)
+    def extract_text_from_webpage(self, url_address: str) -> Dict[str, str or Dict]:
+        response = self.__send_get_request(url=url_address)
+
+        html_file_path = self.__write_dom_object_to_file(content=response)
+
+        soup = BeautifulSoup(html_file_path)
+        BeautifulSoup()
         test = ''.join([repr(string) for string in soup.stripped_strings])
 
-        return {}
+        return {'webpage': test}
