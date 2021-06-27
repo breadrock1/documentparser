76edf04ab12fb2c6530c35be11759c8debf6a77f
breadrock <breadrock1@gmail.com>
2021-06-27 14:46:27 +0300

    Has been fixed tests

--- Test/test_pdf_parser.py
+++ Test/test_pdf_parser.py
@@ -3,7 +3,7 @@ from Parsers.Documents.PdfParser import PdfParser
 
 
 def test_pdf_parser():
-    pdfParser = PdfParser()
     path_to_file = (Path() / 'TestFiles' / 'test_pdf_file.pdf').absolute()
+    pdfParser = PdfParser(file_path=path_to_file)
 
-    assert pdfParser.extract_text_from_file(path_to_file)
+    assert pdfParser.extract_text_from_file()
--- Test/test_webpage_parser.py
+++ Test/test_webpage_parser.py
@@ -1,13 +1,18 @@
+from typing import Dict, Any
+
 from Parsers.WebPages.WebPageParser import WebPageParser
 
 
-def test_pdf_parser():
-    webpageParser = WebPageParser()
+def test_webpage(url: str) -> Dict[str, Any]:
+    webpageParser = WebPageParser(url_address=url)
+    content_data = webpageParser.extract_text_from_webpage()
 
+    return content_data
+
+
+def test_pdf_parser():
     test_url_1 = "https://career.habr.com/muravevstas"
     test_url_2 = "https://spb.hh.ru/resume/6b0697beff057e1f1d0039ed1f3343476d4c4c"
 
-    text_data_1 = webpageParser.extract_text_from_webpage(url_address=test_url_1)
-    text_data_2 = webpageParser.extract_text_from_webpage(url_address=test_url_2)
-
-    assert text_data_1 is not None and text_data_2 is not None
+    assert test_webpage(url=test_url_1) is not None \
+           and test_webpage(url=test_url_2) is not None
