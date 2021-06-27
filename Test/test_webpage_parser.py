b2b8f01f55a6a18fa0c1afe4aefe21ca5af390e1
breadrock <breadrock1@gmail.com>
2021-06-27 14:51:11 +0300

    Has been fixed pytest test_webpage_parser

--- Test/test_webpage_parser.py
+++ Test/test_webpage_parser.py
@@ -3,7 +3,7 @@ from typing import Dict, Any
 from Parsers.WebPages.WebPageParser import WebPageParser
 
 
-def test_webpage(url: str) -> Dict[str, Any]:
+def __test_webpage(url: str) -> Dict[str, Any]:
     webpageParser = WebPageParser(url_address=url)
     content_data = webpageParser.extract_text_from_webpage()
 
@@ -14,5 +14,5 @@ def test_pdf_parser():
     test_url_1 = "https://career.habr.com/muravevstas"
     test_url_2 = "https://spb.hh.ru/resume/6b0697beff057e1f1d0039ed1f3343476d4c4c"
 
-    assert test_webpage(url=test_url_1) is not None \
-           and test_webpage(url=test_url_2) is not None
+    assert __test_webpage(url=test_url_1) is not None \
+           and __test_webpage(url=test_url_2) is not None
