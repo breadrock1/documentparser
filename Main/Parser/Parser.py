31ba618609e8e7bc23f8fdb81714e986e1111131
breadrock <breadrock1@gmail.com>
2021-07-05 23:22:55 +0300

    Has been added ability to launch Parser module

---
+++ Main/Parser/Parser.py
@@ -0,0 +1,12 @@
+from typing import Dict
+
+
+class Parser(object):
+    def __init__(self):
+        self.candidate = dict()
+
+    def parse(self, text_data: str) -> Dict:
+
+
+
+        return self.candidate
